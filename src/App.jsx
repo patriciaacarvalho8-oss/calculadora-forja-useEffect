8f6ace67781a57d1f2cb6d8739b739c39fd74af4
patriciaacarvalho8-oss <patricia.a.carvalho8@aluno.senai.br>
2025-11-13 16:03:51 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [n1, setN1] = useState()
+  const [n2, setN2] = useState()
+  const [respSoma, setRespSoma] = useState()
+  const [respSub, setRespSub] = useState()
+  const [respMult, setRespMult] = useState()
+  const [respDiv, setRespDiv] = useState()
+
+
+  const [error, setError] = useState('')
+
+
+
+    useEffect(() =>{
+      if(n1 !== undefined && n2 !== undefined && !isNaN(n1) && !isNaN(n2)){
+        setRespSoma(parseFloat(n1) + parseFloat(n2))
+        setRespSub(parseFloat(n1) - parseFloat(n2))
+        setRespMult(parseFloat(n1) * parseFloat(n2))
+        setRespDiv(parseFloat(n1) / parseFloat(n2))
+
+        
+        setError('Digite valores númericos para calcular')
+      }else{
+        setError('Digite valores númericos para calcular')
+      }
+    }, [n1, n2])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.wrapAll}>
+      <h1 className={styles.title}>Calculadora</h1>
+      <p>{error}</p>
+      <div className={styles.wrap}>
+        <input type="number" onChange={(e) => setN1(e.target.value)} value={n1} placeholder='Insira um número'/>
+        <input type="number" onChange={(e) => setN2(e.target.value)} value={n2} placeholder='Insira outro número'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className={styles.wrapResps}>
+        <h2>Resultado da Soma</h2>
+        <h3>{respSoma}</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className={styles.wrapResps}>
+        <h2>Resultado da Subtração</h2>
+        <h3>{respSub}</h3>
+      </div>
+      
+      <div className={styles.wrapResps}>
+        <h2>Resultado da Multiplicação</h2>
+        <h3>{respMult}</h3>
+      </div>
+
+      
+      <div className={styles.wrapResps}>
+        <h2>Resultado da Divisão</h2>
+        <h3>{respDiv}</h3>
+      </div>
+    </div>
   )
 }
 
